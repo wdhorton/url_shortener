0ead1664af5b1bf220a726b58dfbacc76a30e331
William Horton <wdt.horton@gmail.com>
2015-10-14 14:13:00 -0400

    Initialize url_shortener and add User model.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UrlShortener::Application.config.secret_key_base = 'a0c005545bbc14a3e002380586c40c0450b9356d10f81e4b15ebd8594f3a7f5bf13ac9804bae58e03b5dd71773e5ee0d0b69847f9d19ae639714f4996c787a78'

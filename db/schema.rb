32c1a74b5de0b775477049302abe30dc82c7f314
William Horton <wdt.horton@gmail.com>
2015-10-14 14:20:21 -0400

    Create shortened_urls table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151014175854) do
+ActiveRecord::Schema.define(version: 20151014181543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "shortened_urls", force: true do |t|
+    t.string   "long_url"
+    t.string   "short_url"
+    t.integer  "submitter_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "shortened_urls", ["short_url"], name: "index_shortened_urls_on_short_url", unique: true, using: :btree
+  add_index "shortened_urls", ["submitter_id"], name: "index_shortened_urls_on_submitter_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email"
